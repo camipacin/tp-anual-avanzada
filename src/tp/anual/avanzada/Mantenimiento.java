54c1b886bce310969be444331184f64f45692713
Pablo <projas@live.com.ar>
2018-04-02 13:44:08 -0300

    Getters y Setters de idPabellon

--- src/tp/anual/avanzada/Mantenimiento.java
+++ src/tp/anual/avanzada/Mantenimiento.java
@@ -3,6 +3,14 @@ package tp.anual.avanzada;
 public class Mantenimiento {
 	
 	private Pabellon idpabellon;
+
+	public Pabellon getIdpabellon() {
+		return idpabellon;
+	}
+
+	public void setIdpabellon(Pabellon idpabellon) {
+		this.idpabellon = idpabellon;
+	}
 	
 	
 }
